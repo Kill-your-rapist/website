7dae21fed1a573db552c17c91b3b0bc795bfac23
Jurien Hamaker <jurien@employes.nl>
2023-04-19 16:07:41 +0200

    chore: update title

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,14 +1,14 @@
 export default function Head() {
     return (
         <>
-            <title>Kill Your Rapist | Barn Influence</title>
+            <title>Kill Your Kicak Rapist | Barn Influence</title>
             <meta
                 content="width=device-width, initial-scale=1"
                 name="viewport"
             />
             <meta
                 name="description"
-                content="Kill your rapist | Barn Influence"
+                content="Kill your local rapist | Barn Influence"
             />
             <link rel="icon" href="/favicon.ico" />
         </>
