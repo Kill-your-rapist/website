9aadd7a84e5db890b4d2ce3f97fe59a0fa75a32b
Jurien Hamaker <whoami@jurien.dev>
2023-04-19 09:44:10 +0200

    feat: initial
    chore: update page title
    
    
    chore: website
    
    
    feat: use embedded player
    
    
    chore: use safe-area

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+var conventions = require('./node_modules/@paperless/conventions/prettier.config');
+
+module.exports = Object.assign(conventions, {
+    tailwindConfig: './tailwind.config.js',
+});
