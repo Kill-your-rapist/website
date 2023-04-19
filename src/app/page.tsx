5264ea92f28812f41220e9c997e0851027387bb3
Jurien Hamaker <jurien@employes.nl>
2023-04-19 16:20:48 +0200

    Cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import './page.scss';
 export default function Home() {
     return (
         <main>
-            <div className="flex h-[80vh] items-center justify-center">
+            <div className="flex flex-1 items-center justify-center">
                 <KillYourRapist />
             </div>
-            <div className="flex w-full flex-col items-center justify-center gap-4 text-center md:max-w-[60vw]">
+            <div className="flex w-full flex-col items-center justify-center gap-4 text-center md:max-w-[40vw]">
                 <Image src={BarnInfluence} alt="Barn Influence" />
 
-                <span className="text-gray-800">
+                <span className="text-xs text-gray-800">
                     We do not support the actual act of killing, Kill your local
                     rapist is not more than a metaphor to remind the people that
                     no means no. We do not take credit for any type of behaviour
